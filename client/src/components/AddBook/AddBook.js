b15c216d4a9d6116e7eeff022c1749f3253a6971
FranciscoDiazPaccot73 <fran.diazpaccot@gmail.com>
2020-11-20 20:56:11 -0300

    add books

--- client/src/components/AddBook/AddBook.js
+++ client/src/components/AddBook/AddBook.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { graphql } from 'react-apollo';
 import {flowRight as compose} from 'lodash';
 import className from 'classname';
@@ -7,14 +7,35 @@ import { getAuthorsQry, addBookMutation, getBooksQry } from '../../utils/queries
 
 import './styles/index.scss';
 
-const AddBook = ({ getAuthors, addBook, showAddBook }) => {
+const AddBook = ({ getAuthors, addBook, showAddBook, reset }) => {
   const { loading, authors } = getAuthors;
   const [name, setName] = useState('');
   const [genre, setGenre] = useState('');
-  const [authorId, setAuthor] = useState('');
+  const [authorId, setAuthor] = useState('0');
+  const [canSend, setCanSend] = useState(false);
   const addBookClasses = className('add-book', {
     'add-book__open': showAddBook,
   });
+  const buttonClasses = className('add-book__button', {
+    'add-book__button--disabled': !canSend,
+  })
+
+  useEffect( () => {
+    if ( name !== '' && genre !== '' && authorId !== '' && authorId !== '0') {
+      setCanSend(true)
+    } else {
+      setCanSend(false)
+    }
+
+    if (reset) resetValues();
+
+  }, [authorId, genre, name, reset]);
+
+  const resetValues = () => {
+    setName('');
+    setAuthor('0');
+    setGenre('');
+  }
 
   const displayAuthors = () => (
     authors.map(author => <option key={ author.id } value={author.id}>{ author.name }</option> )
@@ -24,33 +45,51 @@ const AddBook = ({ getAuthors, addBook, showAddBook }) => {
     <option disabled>Loading authors</option>
   );
 
+  const handleChange = (field, value) => {
+    switch (field) {
+      case 'Book':
+        setName(value);
+        break;
+      case 'Genre':
+        setGenre(value);
+        break;
+      case 'Author':
+        setAuthor(value);
+        break
+      default:
+        break;
+    }
+  }
+
   const submitForm = e => {
+    if (!canSend) return;
     e.preventDefault();
     const data = {name, genre, authorId}
     addBook({
       variables: data,
       refetchQueries: [{ query: getBooksQry }]
     });
+    resetValues();
   }
 
-  return(
-    <form id="add-book" className={addBookClasses} onSubmit={submitForm}>
+   return(
+    <form id="add-book" className={addBookClasses}>
       <div className="field">
           <div className="add-book__label">Book name:</div>
-          <input type="text" onChange={e => setName(e.target.value)} />
+          <input type="text" value={name} onChange={e => handleChange('Book', e.target.value)} />
       </div>
       <div className="field">
           <div className="add-book__label">Genre:</div>
-          <input type="text" onChange={e => setGenre(e.target.value)} />
+          <input type="text" value={genre} onChange={e => handleChange('Genre', e.target.value)} />
       </div>
       <div className="field">
           <div className="add-book__label">Author:</div>
-          <select onChange={e => setAuthor(e.target.value)}>
-              <option>Select author</option>
+          <select value={authorId} onChange={e => handleChange('Author', e.target.value)}>
+              <option value='0' >Select author</option>
               {loading ? emptyState() : displayAuthors()}
           </select>
       </div>
-      <div className="add-book__button">Add Book</div>
+      <div className={buttonClasses} onClick={submitForm}>Add Book</div>
     </form>
   )
 }
--- client/src/components/BookList/BookList.js
+++ client/src/components/BookList/BookList.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { graphql } from 'react-apollo';
+import className from 'classname';
 
 import { getBooksQry } from '../../utils/queries/queries';
 
@@ -14,6 +15,11 @@ const BookList = ({ data }) => {
   const { loading, books, ...rest } = data;
   const [selectedBook, selectBook] = useState(null);
   const [showAddBook, setAddBook] = useState(false);
+  const [shouldReset, setReset] = useState(true);
+
+  const buttonClasses = className('book__add-button', {
+    'book__add-button--opened': showAddBook,
+  });
   
   const handleSelectBook = (value) => {
     if (value === selectedBook) selectBook(null);
@@ -22,19 +28,20 @@ const BookList = ({ data }) => {
 
   const handleAddClick = () => {
     setAddBook(!showAddBook);
+    setReset(!shouldReset);
   }
 
   return(
     <div className="book">
       <div className="book-container__left">
-        <div className="book__add-button" onClick={handleAddClick}>{showAddBook ? 'Close' : 'Add Book'}</div>
+        <div className={buttonClasses} onClick={handleAddClick}>{showAddBook ? 'Close' : 'Add Book'}</div>
         <h1>My Reading List</h1>
         <ul id="book-list" className="book-list">
           {loading ? <EmptyState /> : <DisplayBooks selectedBook={selectedBook} books={books} selectBook={handleSelectBook} />}
         </ul>
       </div>
       <BookDetails selectedBook={selectedBook} selectBook={handleSelectBook} />
-      <AddBook showAddBook={showAddBook} />
+      <AddBook showAddBook={showAddBook} reset={shouldReset} />
     </div>
   );
 }
